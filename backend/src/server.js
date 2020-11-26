31c1207ae09f41434b7808fbcbcb766337ac299b
DESKTOP-A43AVN4\kyong <lklklnm@daum.net>
2020-11-27 03:01:49 +0900

    Add connect db, route

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,28 @@ require('dotenv').config();
 const koa = require('koa');
 const koaRouter = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 const api = require('./api');
+const db = require('./model');
 
 function run() {
     const app = new koa();
     const router = new koaRouter();
 
     app.use(bodyParser());
+    app.use(cors());
+
+    router.use('/api', api.routes());
+
+    app.use(router.routes());
+    app.use(router.allowedMethods());
 
     const port = process.env.PORT || 4000;
 
-    app.listen(port, () => console.log(`server stared to port ${port}`));
-
+    db.sequelize.sync()
+        .then(() => {
+            app.listen(port, () => console.log(`server stared to port ${port}`));
+        })
 };
 
 run();
